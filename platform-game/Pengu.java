da40ef7f936739640c0baf47fc48367a606f87f2
maxwelllu3 <blu@lcs.on.ca>
2019-05-07 15:24:58 -0400

    Got basic platformer working

--- platform-game/Pengu.java
+++ platform-game/Pengu.java
@@ -9,7 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pengu extends Actor
 {
     private int speed = 7;
-    
+    private int vSpeed = 0;
+    private int acceleration = 2;
     /**
      * Act - do whatever the Pengu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,6 +18,7 @@ public class Pengu extends Actor
     public void act() 
     {
         checkKeys();
+        checkFall();
     }
 
     private void checkKeys()
@@ -29,8 +31,40 @@ public class Pengu extends Actor
             setImage("pengu-right.png");
             moveRight();
         }
+        if (Greenfoot.isKeyDown("up") && onGround() == true) {
+            jump();
+        }
     }
 
+    public void jump()
+    {
+        vSpeed = -16;
+        fall();
+    }
+    
+    public void checkFall()
+    {
+        if(onGround()) {
+            vSpeed = 0;
+        }
+        else {
+            fall();
+        }
+    }
+        
+    
+    public boolean onGround()
+    {
+        Actor under = getOneObjectAtOffset(0, getImage().getHeight() / 2, Ground.class);
+        return under != null;
+    }
+    
+    public void fall()
+    {
+        setLocation (getX(), getY() + vSpeed);
+        vSpeed = vSpeed + acceleration;
+    }
+    
     public void moveRight()
     {
         setLocation(getX() + speed, getY());
