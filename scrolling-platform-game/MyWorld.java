42f9f84832f01657998b43f453e355469a6dc74c
Russell Gordon <rgordon@lcs.on.ca>
2019-05-08 14:42:18 -0400

    Set world size and background image

---
+++ scrolling-platform-game/MyWorld.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Template for a side-scrolling platform game.
+ * 
+ * @author R. Gordon
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 640x480 cells with a cell size of 1x1 pixels.
+        // Final argument of 'false' means that actors in the world are not restricted to the world boundary.
+        // See: https://www.greenfoot.org/files/javadoc/greenfoot/World.html#World-int-int-int-boolean-
+        super(640, 480, 1, false); 
+    }
+}
