05374084143f96beaf481df948b517956e232867
Russell Gordon <rgordon@lcs.on.ca>
2019-05-09 14:48:25 -0400

    Fix bug where hero could run on sub ground tiles

--- scrolling-platform-game/GroundBelow.java
+++ scrolling-platform-game/GroundBelow.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class GroundBelow extends Platform
+public class GroundBelow extends Decoration
 {
     /**
      * Constructor
