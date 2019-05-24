eae9a5f32364e6d17897f324a8436a8c8d631bb9
maxwelllu3 <blu@lcs.on.ca>
2019-05-24 15:25:25 -0400

    Got background working and got sound working

---
+++ scrolling-platform-game/Sonic.java
@@ -0,0 +1,512 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * This is the class for the "main character" in the action.
+ * 
+ * @author R. Gordon
+ * @version May 8, 2019
+ */
+public class Sonic extends Actor
+{
+    /**
+     * Instance variables
+     * 
+     * These are available for use in any method below.
+     */    
+    // Horizontal speed (change in horizontal position, or delta X)
+    private int deltaX = 4;
+
+    // Vertical speed (change in vertical position, or delta Y)
+    private int deltaY = 4;
+
+    // Acceleration for falls
+    private int acceleration = 2;
+
+    // Strength of a jump
+    private int jumpStrength = -24;
+
+    // Track current theoretical position in wider "scrollable" world
+    private int currentScrollableWorldXPosition;
+
+    // Track whether game is over or not
+    private boolean isGameOver;
+
+    // Constants to track vertical direction
+    private static final String JUMPING_UP = "up";
+    private static final String JUMPING_DOWN = "down";
+    private String verticalDirection;
+
+    // Constants to track horizontal direction
+    private static final String FACING_RIGHT = "right";
+    private static final String FACING_LEFT = "left";
+    private String horizontalDirection;
+
+    // For walking animation
+    private GreenfootImage walkingRightImages[];
+    private GreenfootImage walkingLeftImages[];
+    private static final int WALK_ANIMATION_DELAY = 8;
+    private static final int COUNT_OF_WALKING_IMAGES = 2;
+    private int walkingFrames;
+
+    /**
+     * Constructor
+     * 
+     * This runs once when the Sonic object is created.
+     */
+    Sonic(int startingX)
+    {
+        // Set where sonic begins horizontally
+        currentScrollableWorldXPosition = startingX;
+
+        // Game on
+        isGameOver = false;
+
+        // First jump will be in 'down' direction
+        verticalDirection = JUMPING_DOWN;
+
+        // Facing right to start
+        horizontalDirection = FACING_RIGHT;
+
+        // Set image
+        setImage("Sonic_Ball.png");
+
+        // Initialize the 'walking' arrays
+        walkingRightImages = new GreenfootImage[COUNT_OF_WALKING_IMAGES];
+        walkingLeftImages = new GreenfootImage[COUNT_OF_WALKING_IMAGES];
+
+        // Load walking images from disk
+        for (int i = 0; i < walkingRightImages.length; i++)
+        {
+            walkingRightImages[i] = new GreenfootImage("Sonic_WalkRight-" + i + ".png");
+
+            // Create left-facing images by mirroring horizontally
+            walkingLeftImages[i] = new GreenfootImage(walkingRightImages[i]);
+            walkingLeftImages[i].mirrorHorizontally();
+        }
+
+        // Track animation frames for walking
+        walkingFrames = 0;
+    }
+
+    /**
+     * Act - do whatever the Sonic wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkKeys();
+        checkFall();
+        if (!isGameOver)
+        {
+            checkGameOver();
+        }
+    }
+
+    /**
+     * Respond to keyboard action from the user.
+     */
+    private void checkKeys()
+    {
+        // Walking keys
+        if (Greenfoot.isKeyDown("left") && !isGameOver)
+        {
+            moveLeft();
+        }
+        else if (Greenfoot.isKeyDown("right") && !isGameOver)
+        {
+            moveRight();
+        }
+        else
+        {
+            // Standing still; reset walking animation
+            walkingFrames = 0;
+        }
+
+        // Jumping
+        if (Greenfoot.isKeyDown("up") && !isGameOver)
+        {
+            // Only able to jump when on a solid object
+            if (onPlatform())
+            {
+                jump();
+            }
+        }
+    }
+
+    /**
+     * Should the sonic be falling right now?
+     */
+    public void checkFall()
+    {
+        if (onPlatform())
+        {
+            // Stop falling
+            deltaY = 0;
+
+            // Set image
+            if (horizontalDirection == FACING_RIGHT && Greenfoot.isKeyDown("right") == false)
+            {
+                setImage("Sonic_Right.png");
+            }
+            else if (horizontalDirection == FACING_LEFT && Greenfoot.isKeyDown("left") == false)
+            {
+                setImage("Sonic_Left.png");
+            }
+
+            // Get a reference to any object that's created from a subclass of Platform,
+            // that is below (or just below in front, or just below behind) the sonic
+            Actor directlyUnder = getOneObjectAtOffset(0, getImage().getHeight() / 2, Platform.class);
+            Actor frontUnder = getOneObjectAtOffset(getImage().getWidth() / 3, getImage().getHeight() / 2, Platform.class);
+            Actor rearUnder = getOneObjectAtOffset(0 - getImage().getWidth() / 3, getImage().getHeight() / 2, Platform.class);
+
+            // Bump the sonic back up so that they are not "submerged" in a platform object
+            if (directlyUnder != null)
+            {
+                int correctedYPosition = directlyUnder.getY() - directlyUnder.getImage().getHeight() / 2 - this.getImage().getHeight() / 2;
+                setLocation(getX(), correctedYPosition);
+            }
+            if (frontUnder != null)
+            {
+                int correctedYPosition = frontUnder.getY() - frontUnder.getImage().getHeight() / 2 - this.getImage().getHeight() / 2;
+                setLocation(getX(), correctedYPosition);
+            }
+            if (rearUnder != null)
+            {
+                int correctedYPosition = rearUnder.getY() - rearUnder.getImage().getHeight() / 2 - this.getImage().getHeight() / 2;
+                setLocation(getX(), correctedYPosition);
+            }
+        }
+        else
+        {
+            fall();
+        }
+    }
+
+    /**
+     * Is the sonic currently touching a solid object? (any subclass of Platform)
+     */
+    public boolean onPlatform()
+    {
+        // Get an reference to a solid object (subclass of Platform) below the sonic, if one exists
+        Actor directlyUnder = getOneObjectAtOffset(0, getImage().getHeight() / 2, Platform.class);
+        Actor frontUnder = getOneObjectAtOffset(getImage().getWidth() / 3, getImage().getHeight() / 2, Platform.class);
+        Actor rearUnder = getOneObjectAtOffset(0 - getImage().getWidth() / 3, getImage().getHeight() / 2, Platform.class);
+
+        // If there is no solid object below (or slightly in front of or behind) the sonic...
+        if (directlyUnder == null && frontUnder == null && rearUnder == null)
+        {
+            return false;   // Not on a solid object
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /**
+     * Make the sonic jump.
+     */
+    public void jump()
+    {
+        // Track vertical direction
+        verticalDirection = JUMPING_UP;
+
+        // Set image
+        if (horizontalDirection == FACING_RIGHT)
+        {
+            setImage("Sonic_JumpUpRight.png");
+        }
+        else
+        {
+            setImage("Sonic_JumpUpLeft.png");
+        }
+
+        // Change the vertical speed to the power of the jump
+        deltaY = jumpStrength;
+
+        // Make the character move vertically 
+        fall();
+    }
+
+    /**
+     * Make the sonic fall.
+     */
+    public void fall()
+    {
+        // See if direction has changed
+        if (deltaY > 0)
+        {
+            verticalDirection = JUMPING_DOWN;
+
+            // Set image
+            if (horizontalDirection == FACING_RIGHT)
+            {
+                setImage("Sonic_JumpDownRight.png");
+            }
+            else
+            {
+                setImage("Sonic_JumpDownLeft.png");
+            }
+        }
+
+        // Fall (move vertically)
+        int newVisibleWorldYPosition = getY() + deltaY;
+        setLocation(getX(), newVisibleWorldYPosition );
+
+        // Accelerate (fall faster next time)
+        deltaY = deltaY + acceleration;
+    }
+
+    /**
+     * Animate walking
+     */
+    private void animateWalk(String direction)
+    {
+        // Track walking animation frames
+        walkingFrames += 1;
+
+        // Get current animation stage
+        int stage = walkingFrames / WALK_ANIMATION_DELAY;
+
+        // Animate
+        if (stage < walkingRightImages.length)
+        {
+            // Set image for this stage of the animation
+            if (direction == FACING_RIGHT)
+            {
+                setImage(walkingRightImages[stage]);
+            }
+            else
+            {
+                setImage(walkingLeftImages[stage]);
+            }
+        }
+        else
+        {
+            // Start animation loop from beginning
+            walkingFrames = 0;
+        }
+    }
+
+    /**
+     * Move the sonic to the right.
+     */
+    public void moveRight()
+    {
+        // Track direction
+        horizontalDirection = FACING_RIGHT;
+
+        // Set image 
+        if (onPlatform())
+        {
+            animateWalk(horizontalDirection);
+        }
+        else
+        {
+            // Set appropriate jumping image
+            if (verticalDirection == JUMPING_UP)
+            {
+                setImage("Sonic_JumpUpRight.png");
+            }
+            else
+            {
+                setImage("Sonic_JumpDownRight.png");
+            }
+        }
+
+        // Get object reference to world
+        SonicWorld world = (SonicWorld) getWorld(); 
+
+        // Decide whether to actually move, or make world's tiles move
+        if (currentScrollableWorldXPosition < world.HALF_VISIBLE_WIDTH)
+        {
+            // HERO IS WITHIN EXTREME LEFT PORTION OF SCROLLABLE WORLD
+            // So... actually move the actor within the visible world.
+
+            // Move to right in visible world
+            int newVisibleWorldXPosition = getX() + deltaX;
+            setLocation(newVisibleWorldXPosition, getY());
+
+            // Track position in wider scrolling world
+            currentScrollableWorldXPosition = getX();
+        }
+        else if (currentScrollableWorldXPosition + deltaX * 2 > world.SCROLLABLE_WIDTH - world.HALF_VISIBLE_WIDTH)
+        {
+            // HERO IS WITHIN EXTREME RIGHT PORTION OF SCROLLABLE WORLD
+            // So... actually move the actor within the visible world.
+
+            // Allow movement only when not at edge of world
+            if (currentScrollableWorldXPosition < world.SCROLLABLE_WIDTH - this.getImage().getWidth() / 2)
+            {
+                // Move to right in visible world
+                int newVisibleWorldXPosition = getX() + deltaX;
+                setLocation(newVisibleWorldXPosition, getY());
+
+                // Track position in wider scrolling world
+                currentScrollableWorldXPosition += deltaX;
+            }
+            else
+            {
+                isGameOver = true;
+                world.setGameOver();
+
+                // Tell the user game is over
+                world.showText("LEVEL COMPLETE", world.getWidth() / 2, world.getHeight() / 2);
+            }
+
+        }
+        else
+        {
+            // HERO IS BETWEEN LEFT AND RIGHT PORTIONS OF SCROLLABLE WORLD
+            // So... we move the other objects to create illusion of sonic moving
+
+            // Track position in wider scrolling world
+            currentScrollableWorldXPosition += deltaX;
+
+            // Get a list of all platforms (objects that need to move
+            // to make sonic look like they are moving)
+            List<Platform> platforms = world.getObjects(Platform.class);
+
+            // Move all the platform objects to make it look like sonic is moving
+            for (Platform platform : platforms)
+            {
+                // Platforms move left to make sonic appear to move right
+                platform.moveLeft(deltaX);
+            }
+
+            // Get a list of all decorations (objects that need to move
+            // to make sonic look like they are moving)
+            List<Decoration> decorations = world.getObjects(Decoration.class);
+
+            // Move all the decoration objects to make it look like sonic is moving
+            for (Decoration decoration: decorations)
+            {
+                // Platforms move left to make sonic appear to move right
+                decoration.moveLeft(deltaX);
+            }
+
+            
+        }   
+
+    }
+
+    /**
+     * Move the sonic to the left.
+     */
+    public void moveLeft()
+    {
+        // Track direction
+        horizontalDirection = FACING_LEFT;
+
+        // Set image 
+        if (onPlatform())
+        {
+            animateWalk(horizontalDirection);
+        }
+        else
+        {
+            // Set appropriate jumping image
+            if (verticalDirection == JUMPING_UP)
+            {
+                setImage("Sonic_JumpUpLeft.png");
+            }
+            else
+            {
+                setImage("Sonic_JumpDownLeft.png");
+            }
+        }
+
+        // Get object reference to world
+        SonicWorld world = (SonicWorld) getWorld(); 
+
+        // Decide whether to actually move, or make world's tiles move
+        if (currentScrollableWorldXPosition - deltaX < world.HALF_VISIBLE_WIDTH)
+        {
+            // HERO IS WITHIN EXTREME LEFT PORTION OF SCROLLABLE WORLD
+            // So... actually move the actor within the visible world.
+
+            // Don't let sonic go off left edge of scrollable world 
+            // (Allow movement only when not at left edge)
+            if (currentScrollableWorldXPosition > 0)
+            {
+                // Move left in visible world
+                int newVisibleWorldXPosition = getX() - deltaX;
+                setLocation(newVisibleWorldXPosition, getY());
+
+                // Track position in wider scrolling world
+                currentScrollableWorldXPosition = getX();
+            }            
+        }
+        else if (currentScrollableWorldXPosition + deltaX * 2 > world.SCROLLABLE_WIDTH - world.HALF_VISIBLE_WIDTH)
+        {
+            // HERO IS WITHIN EXTREME RIGHT PORTION OF SCROLLABLE WORLD
+            // So... actually move the actor within the visible world.
+
+            // Move left in visible world
+            int newVisibleWorldXPosition = getX() - deltaX;
+            setLocation(newVisibleWorldXPosition, getY());
+
+            // Track position in wider scrolling world
+            currentScrollableWorldXPosition -= deltaX;
+        }        
+        else
+        {
+            // HERO IS BETWEEN LEFT AND RIGHT PORTIONS OF SCROLLABLE WORLD
+            // So... we move the other objects to create illusion of sonic moving
+
+            // Track position in wider scrolling world
+            currentScrollableWorldXPosition -= deltaX;
+
+            // Get a list of all platforms (objects that need to move
+            // to make sonic look like they are moving)
+            List<Platform> platforms = world.getObjects(Platform.class);
+
+            // Move all the platform objects at same speed as sonic
+            for (Platform platform : platforms)
+            {
+                // Platforms move right to make sonic appear to move left
+                platform.moveRight(deltaX);
+            }
+
+            // Get a list of all decorations (objects that need to move
+            // to make sonic look like they are moving)
+            List<Decoration> decorations = world.getObjects(Decoration.class);
+
+            // Move all the decoration objects to make it look like sonic is moving
+            for (Decoration decoration: decorations)
+            {
+                // Platforms move right to make sonic appear to move left
+                decoration.moveRight(deltaX);
+            }
+
+            
+        } 
+
+    }
+
+    /**
+     * When the sonic falls off the bottom of the screen,
+     * game is over. We must remove them.
+     */
+    public void checkGameOver()
+    {
+        // Get object reference to world
+        SonicWorld world = (SonicWorld) getWorld(); 
+
+        // Vertical position where sonic no longer visible
+        int offScreenVerticalPosition = (world.getHeight() + this.getImage().getHeight() / 2);
+
+        // Off bottom of screen?
+        if (this.getY() > offScreenVerticalPosition)
+        {
+            // Remove the sonic
+            isGameOver = true;
+            world.setGameOver();
+            world.removeObject(this);
+
+            // Tell the user game is over
+            world.showText("GAME OVER", world.getWidth() / 2, world.getHeight() / 2);
+        }
+    }
+}
---
+++ scrolling-platform-game/SonicWorld.java
@@ -0,0 +1,303 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A side-scrolling platform game.
+ * 
+ * @author Maxwell B. Lu
+ * @version May 16, 2019
+ */
+public class SonicWorld extends World
+{
+    /**
+     * Instance variables
+     * 
+     * These are available for use in any method below.
+     */    
+    // Tile size in pixels for world elements (blocks, clouds, etc)
+    // TO STUDENTS: Modify if your game's tiles have different dimensions
+    private static final int TILE_SIZE = 32;
+    private static final int HALF_TILE_SIZE = TILE_SIZE / 2;
+
+    // World size constants
+    // TO STUDENTS: Modify only if you're sure
+    //              Should be a resolution that's a multiple of TILE_SIZE
+    private static final int VISIBLE_WIDTH = 640;
+    private static final int VISIBLE_HEIGHT = 480;
+
+    // Additional useful constants based on world size
+    public static final int HALF_VISIBLE_WIDTH = VISIBLE_WIDTH / 2;
+    private static final int HALF_VISIBLE_HEIGHT = VISIBLE_HEIGHT / 2;
+
+    // Defining the boundaries of the scrollable world
+    // TO STUDENTS: Modify SCROLLABLE_WIDTH if you wish to have a longer level
+    public static final int SCROLLABLE_WIDTH = VISIBLE_WIDTH * 3;
+    private static final int SCROLLABLE_HEIGHT = VISIBLE_HEIGHT;
+
+    // Sonic
+    Sonic theSonic;
+
+    // Track whether game is on
+    private boolean isGameOver;
+    /**
+     * Constructor for objects of class SonicWorld.
+     */
+    public SonicWorld()
+    {    
+        // Create a new world with 640x480 cells with a cell size of 1x1 pixels.
+        // Final argument of 'false' means that actors in the world are not restricted to the world boundary.
+        // See: https://www.greenfoot.org/files/javadoc/greenfoot/World.html#World-int-int-int-boolean-
+        super(VISIBLE_WIDTH, VISIBLE_HEIGHT, 1, false);
+
+        // Set up the starting scene
+        setup();
+
+        // Game on
+        isGameOver = false;
+    }
+
+    /**
+     * Set up the entire world.
+     */
+    private void setup()
+    {
+        // TO STUDENTS: Add, revise, or remove methods as needed to define your own game's world
+        // addLeftGround();
+        //  addFences();
+        //  addFloatingRockSteps();
+        //  addClouds();
+        //  addRightGround();
+        addSonic();
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 7 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 4 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 17; i += 1)
+        {
+            int x = HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 14 * TILE_SIZE + HALF_TILE_SIZE;
+            Ground ground = new Ground(x, y);
+            addObject(ground, x, y);
+        }
+
+        for (int i = 0; i < 18; i += 1)
+        {
+            int x = 11 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 2 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 17 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 11 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 22 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 9 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 9; i += 1)
+        {
+            int x = 12 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 6 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 29 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 4 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 33 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 6 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 37 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 8 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 33 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 10 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 10; i += 1)
+        {
+            int x = 37 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 12 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 47 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 10 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 4; i += 1)
+        {
+            int x = 51 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 8 * TILE_SIZE + HALF_TILE_SIZE;
+            FloatingRock floatingrock = new FloatingRock(x, y);
+            addObject(floatingrock, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 6 * TILE_SIZE + HALF_TILE_SIZE;
+            Ground ground = new Ground(x, y);
+            addObject(ground, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 7 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 8 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 9 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 10 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 11 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 12 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 13 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 14 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            int x = 56 * TILE_SIZE + HALF_TILE_SIZE + i * TILE_SIZE;
+            int y = 15 * TILE_SIZE + HALF_TILE_SIZE;
+            GroundBelow groundbelow = new GroundBelow(x, y);
+            addObject(groundbelow, x, y);
+        }
+
+    }
+
+    /**
+     * Act
+     * 
+     * This method is called approximately 60 times per second.
+     */
+    public void act()
+    {
+
+    }
+
+    /**
+     * Add the hero to the world.
+     */
+    private void addSonic()
+    {
+        // Initial horizontal position
+        int initialX = TILE_SIZE * 3;
+
+        // Instantiate the hero object
+        theSonic = new Sonic(initialX);
+
+        // Add hero in bottom left corner of screen
+        addObject(theSonic, initialX, getHeight() / 4 * 2);
+    }
+
+    /**
+     * Return an object reference to the hero.
+     */
+    public Sonic getSonic()
+    {
+        return theSonic;
+    }
+
+    public void started()
+    {
+        Greenfoot.playSound("music1.mp3");
+    }
+
+    /**
+     * Set game over
+     */
+    public void setGameOver()
+    {
+        isGameOver = true;
+    }
+}
+
